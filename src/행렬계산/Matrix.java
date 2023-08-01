4d53ef5fd1cd1f4c144ca0c6476d893584b466ce
choe taebin <choetaebin1234577@gmail.com>
2023-08-01 14:29:43 +0900

    정리정돈

--- src/행렬계산/Matrix.java
+++ src/행렬계산/Matrix.java
@@ -10,39 +10,41 @@ public class Matrix{//행렬
 	int row; //행 크기
 	int col; //열 크기
 
-	Matrix(RationalNum[][] matrix){
+	public Matrix(RationalNum[][] matrix){
 		this.matrix = matrix;
 		row = matrix.length;
 		col = matrix[0].length;
 	}
-
-	Matrix multiplyToLeft(Matrix counterPart){ //왼쪽에 곱한 결과를 반환
-		RationalNum[][] cMatrix = counterPart.matrix;
-
-		int cRow = cMatrix.length; //상대 행렬 행 크기
-		int cCol = cMatrix[0].length; //상대 행렬 열 크기
-
-		if(col != cRow) {
-			System.out.println("곱의 형태가 맞지 않음");
+	
+	/*
+	 * 넘겨받은 두행렬을 곱해서 결과를 반환한다.
+	 */
+	public static Matrix multiply(Matrix m1, Matrix m2){ //왼쪽에 곱한 결과를 반환
+		
+		if(m1.col != m2.row) {
+			System.out.println("곱의 형태가 맞지 않습니다.");
 			return null;
 		}
 
-		RationalNum[][] result = new RationalNum[row][cCol];//결과를 저장
+		RationalNum[][] result = new RationalNum[m1.row][m2.col];//결과를 저장
 
-		for(int r=0; r<row; r++) {
-			for(int c=0; c<cCol; c++) {
+		for(int r=0; r<m1.row; r++) {
+			for(int c=0; c<m2.col; c++) {
 				//this의 r행벡터와 cMatrix의 c열 벡터를 내적하여 결과 행렬의 r행 c열에 저장 한다.
 				result[r][c] = new RationalNum();
-				for(int i=0; i<col; i++) {
-					result[r][c].add(RationalNum.multiply(matrix[r][i],cMatrix[i][c]));
+				for(int i=0; i<m1.col; i++) {
+					result[r][c].add(RationalNum.multiply(m1.matrix[r][i],m2.matrix[i][c]));
 				}
 			}
 		}
 
 		return new Matrix(result);
 	}
-
-	RationalNum getDeterminant() {
+	
+	/*
+	 * 행렬식을 구하는 함수
+	 */
+	public RationalNum getDeterminant() {
 		if(row != col) {
 			System.out.println("정방 행렬만 행렬식을 구할 수 있습니다.");
 		}
@@ -51,6 +53,7 @@ public class Matrix{//행렬
 		return determinant(0,0,row, isHidden); //0행0열 부터 size가 row인 소행렬 식을 구해서 반환한다.
 	}
 	
+	//소행렬식을 구하는 함수, 재귀적으로 정의
 	private RationalNum determinant(int startRow, int startCol,int size, boolean[] isHidden) {//size*size 크기의 소행렬식을 구함
 		if(size == 1) {//종결 조건
 			return matrix[startRow][startCol];
@@ -82,7 +85,8 @@ public class Matrix{//행렬
 		return result;
 	}
 	
-	private int nextCol(int curCol,boolean[] isHidden) {//가려지지 않은 다음 열을 찾아 주는 함수
+	//가려지지 않은 다음 열을 찾아 주는 함수
+	private int nextCol(int curCol,boolean[] isHidden) {
 		int nextCol = curCol+1;
 		
 		while(nextCol < col) {
@@ -94,13 +98,18 @@ public class Matrix{//행렬
 		
 		return -1; //다음 열 없음
 	}
-
-	void print() {
+	
+	public String toString() {
+		StringBuffer rStr = new StringBuffer();
+		
 		for(int r=0; r<row ; r++) {
 			for(int c=0; c<col; c++) {
-				System.out.print(matrix[r][c] + "\t");
+				rStr.append(matrix[r][c] + "\t");
 			}
-			System.out.println();
+			rStr.append("\n");
 		}
+		
+		return rStr.toString();
 	}
+	
 }

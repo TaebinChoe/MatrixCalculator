3af255200e5f6590c75cbb3955e8cda97da40a1b
choe taebin <choetaebin1234577@gmail.com>
2023-08-02 14:27:35 +0900

    역행렬 기능 추가

--- src/행렬계산/Matrix.java
+++ src/행렬계산/Matrix.java
@@ -48,49 +48,58 @@ public class Matrix{//행렬
 		if(row != col) {
 			System.out.println("정방 행렬만 행렬식을 구할 수 있습니다.");
 		}
-		boolean[] isHidden = new boolean[col]; //가려진 열을 표시
+		boolean[][] isHidden = new boolean[2][col]; //가려진 행 열을 표시 0행-행 1행-열
 		
-		return determinant(0,0,row, isHidden); //0행0열 부터 size가 row인 소행렬 식을 구해서 반환한다.
+		return determinant(row, isHidden); //0행0열 부터 size가 row인 소행렬 식을 구해서 반환한다.
 	}
 	
 	//소행렬식을 구하는 함수, 재귀적으로 정의
-	private RationalNum determinant(int startRow, int startCol,int size, boolean[] isHidden) {//size*size 크기의 소행렬식을 구함
+	private RationalNum determinant(int size, boolean[][] isHidden) {//size*size 크기의 소행렬식을 구함
+		int startRow = 0;//시작 행
+		int startCol = 0;//시작 열
+		
+		while(isHidden[0][startRow]) {//가려지지 않은 최 상단 행을 찾음
+			startRow++;
+		}
+		while(isHidden[1][startCol]) {//가려지지 않은 가장 좌측 열을 찾음
+			startCol++;
+		}
+		
 		if(size == 1) {//종결 조건
 			return matrix[startRow][startCol];
 		}
+		isHidden[0][startRow] = true; //현재행을 가림
 		
 		RationalNum result = new RationalNum();//결과를 저장
 		
-		int curCol = startCol; //startCol은 가려지지 않은애만 들어 올 수 있음
+		int curCol = startCol;
 		for(int i=0; i<size; i++) {//size번의 소행렬식을 구해 더해야함
-			isHidden[curCol] = true; //현재 열 가림
 			
-			int c = 0; //시작 점을 찾기 위함 
-			while(isHidden[c]) {//안 가린 애중 가장 작은 col부터 시작 해야함
-				c++;
-			}
-			
+			isHidden[1][curCol] = true; //현재 열 가림
+		
 			if(i%2 == 0){//홀수번째면 더함
-				result.add(RationalNum.multiply(matrix[startRow][curCol], determinant(startRow+1,c,size-1,isHidden)));
+				result.add(RationalNum.multiply(matrix[startRow][curCol], determinant(size-1,isHidden)));
 				//System.out.println(result);
 			}else {//짝수번째면 뺌
-				result.subtract(RationalNum.multiply(matrix[startRow][curCol], determinant(startRow+1,c,size-1,isHidden)));
+				result.subtract(RationalNum.multiply(matrix[startRow][curCol], determinant(size-1,isHidden)));
 				//System.out.println(result);
 			}
 			
-			isHidden[curCol] = false; //가림 해제
+			isHidden[1][curCol] = false; //가림 해제
 			
 			curCol = nextCol(curCol,isHidden); //다음 가리지 않은 열을 찾음
 		}
+		
+		isHidden[0][startRow] = false; //현재열 가림 해제
 		return result;
 	}
 	
-	//가려지지 않은 다음 열을 찾아 주는 함수
-	private int nextCol(int curCol,boolean[] isHidden) {
+	//가려지지 않은 다음 왼쪽 열을 찾아 주는 함수
+	private int nextCol(int curCol,boolean[][] isHidden) {
 		int nextCol = curCol+1;
 		
 		while(nextCol < col) {
-			if(!isHidden[nextCol]) {//가려 지지 않았다면
+			if(!isHidden[1][nextCol]) {//가려 지지 않았다면
 				return nextCol;
 			}
 			nextCol++;
@@ -98,6 +107,49 @@ public class Matrix{//행렬
 		
 		return -1; //다음 열 없음
 	}
+	
+	/*
+	 * 역행렬을 구해서 반환 하는 함수
+	 */
+	public Matrix getInverse() {
+		RationalNum determinant = getDeterminant(); //행렬식
+		
+		if(row != col) {//정방행렬이 아닌경우
+			System.out.println("정방 행렬만 행렬식을 구할 수 있습니다.");
+			return null;
+		}
+		
+		if(determinant.equals(new RationalNum(1,0))) {//비가역 행렬인 경우
+			System.out.println("비가역 행렬입니다.");
+			return null;
+		}
+		
+		RationalNum[][] adjointArr = new RationalNum[row][col];
+		
+		boolean[][] isHidden = new boolean[2][col]; //가려진 행 열을 표시 0행-행 1행-열
+		
+		for(int r=0; r<row; r++) {
+			
+			for(int c=0; c<col; c++) {
+				//r행 c열 가림
+				isHidden[0][r] = true;
+				isHidden[1][c] = true;
+				//Crc를 계산해서 determinant로 나눈 후 c행 r열에 저장
+				
+				adjointArr[c][r] = RationalNum.divide(determinant(col-1,isHidden),determinant); 
+				if((r+c) %2 == 1) adjointArr[c][r].multiply(new RationalNum(1,-1));
+				
+				//가림 해제
+				isHidden[0][r] = false;
+				isHidden[1][c] = false;
+				
+			}
+		}
+		
+		
+		return new Matrix(adjointArr);
+	}
+	
 	
 	public String toString() {
 		StringBuffer rStr = new StringBuffer();
--- src/행렬계산/TestDriver.java
+++ src/행렬계산/TestDriver.java
@@ -7,14 +7,14 @@ import java.util.StringTokenizer;
 public class TestDriver {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		final int MULTIPLY = 1, DETERMINANT = 2, EXIT = 3;
+		final int MULTIPLY = 1, DETERMINANT = 2, INVERSE = 3,EXIT = 4;
 		StringTokenizer st;
 		int command; //명령
 		RationalNum[][] matrixBuff; //행렬 buffer;
 		Matrix[] matrix; //행렬 배열
 		
 		while(true) {
-			System.out.print("원하시는 항목을 선택해 주세요(1 행렬곱 연산, 2 행렬식 연산, 3 종료): ");
+			System.out.print("원하시는 항목을 선택해 주세요(1 행렬곱 연산, 2 행렬식 연산, 3 역행렬 ,4 종료): ");
 			st = new StringTokenizer(br.readLine());
 			command  = Integer.parseInt(st.nextToken()); //명령을 읽어들임
 
@@ -65,6 +65,25 @@ public class TestDriver {
 
 				System.out.println("행렬식: "+matrix[0].getDeterminant());
 				break;
+			case INVERSE:
+				matrix = new Matrix[1];
+				System.out.println("원하는 행렬의 크기를 입력하시오.(행, 열)");
+				st = new StringTokenizer(br.readLine());//읽어들임
+				matrixBuff = new RationalNum[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())];
+				//입력을 받음
+				System.out.println("행렬을 입력하시오. \n(예시)\n1 2\n3 4");
+				for(int r=0; r<matrixBuff.length; r++) {
+					st = new StringTokenizer(br.readLine());
+					for(int c=0; c<matrixBuff[0].length; c++) {
+						matrixBuff[r][c] = new RationalNum(1,Integer.parseInt(st.nextToken()));
+					}
+				}
+				matrix[0] = new Matrix(matrixBuff); //행렬 객체를 만들어서 저장
+				System.out.println("입력하신 행렬:");
+				System.out.println(matrix[0]);
+
+				System.out.println("역행렬:\n"+matrix[0].getInverse());
+				break;	
 				
 			case EXIT:
 				return;
